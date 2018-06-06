fb4136fd42b986c05f4778f059901ff3d36598ea
Hari Prasanth Chozhan <24803379+Harichozhan@users.noreply.github.com>
2018-06-06 14:42:40 +0530

    Add files via upload

---
+++ servo_3_motors.ino.ino
@@ -0,0 +1,24 @@
+#include <Servo.h>
+int servoPin = 3;
+
+Servo Servo1, Servo2, Servo3;
+void setup() {
+  Servo1.attach(1);
+  Servo2.attach(2);
+  Servo3.attach(3);
+}
+
+void loop() {
+  Servo1.write(0);
+  delay(1000);
+  Servo1.write(180);
+  delay(1000);
+  Servo2.write(0);
+  delay(1000);
+  Servo2.write(180);
+  delay(1000);
+  Servo3.write(0);
+  delay(1000);
+  Servo3.write(180);
+  delay(1000);
+}
