0f983ef74ab90205a19c4f490a8c5c13437cb16c
Hari Prasanth Chozhan <24803379+Harichozhan@users.noreply.github.com>
2018-06-12 23:18:32 +0530

    Add files via upload

---
+++ Water_tank__FR_.ino
@@ -0,0 +1,33 @@
+int floatsensor=3;   
+int led=2;           
+int button = 1; 
+#define CW 7
+#define CCW 8
+void setup() 
+{ 
+Serial.begin(9600); 
+pinMode(floatsensor, INPUT_PULLUP); 
+pinMode (led, OUTPUT); 
+pinMode(CW, OUTPUT); 
+pinMode(CCW, OUTPUT);
+}
+void loop() 
+{ 
+button = digitalRead(floatsensor); 
+  if (button == HIGH) 
+  { 
+    digitalWrite(led, HIGH);
+    Serial.println( "WATER LEVEL - HIGH"); 
+    digitalWrite(CW,HIGH); 
+    delay(1000);
+    digitalWrite(CW, LOW);
+  } 
+  else 
+  { 
+    digitalWrite(led, LOW);
+    Serial.println( "WATER LEVEL - LOW" ); 
+    digitalWrite(CCW, HIGH);
+    delay(1000);
+    digitalWrite(CCW, LOW);
+  } 
+}
---
+++ water_tank_control.ino
@@ -0,0 +1,29 @@
+int floatsensor=3;   
+int led=2;           
+int button = 1; 
+#define CW 7
+void setup() 
+{ 
+Serial.begin(9600); 
+pinMode(floatsensor, INPUT_PULLUP); 
+pinMode (led, OUTPUT); 
+pinMode(CW, OUTPUT); 
+}
+void loop() 
+{ 
+button = digitalRead(floatsensor); 
+  if (button == HIGH) 
+  { 
+    digitalWrite(led, HIGH);
+    Serial.println( "WATER LEVEL - HIGH"); 
+    digitalWrite(CW, LOW);
+  } 
+  else 
+  { 
+    digitalWrite(led, LOW);
+    Serial.println( "WATER LEVEL - LOW" );
+    digitalWrite(CW,HIGH); 
+    delay(1000);
+    digitalWrite(CW, LOW);     
+   } 
+}
